91e93d58bbf3697ff096b05a2577789e39d60591
juliafei <juliafeilau@gmail.com>
2016-04-16 18:29:47 -0400

    NomNom
    Added View Controllers

---
+++ NomNom/ResultsViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ResultsViewController.swift
+//  NomNom
+//
+//  Created by Julia Lau on 4/16/16.
+//  Copyright © 2016 Shirley He. All rights reserved.
+//
+
+import UIKit
+
+class ResultsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
